39574c67a8e9e83f7121069f6f46c1bfef4dd660
niteshsingh1357 <niteshsingh1357@gmail.com>
2023-02-21 20:56:17 +0545

    minimize package

--- index.js
+++ index.js
@@ -11,29 +11,7 @@ function lookupViaCity(city) {
   }
 }
 
-function findPartialMatch(itemsToSearch, searchString) {
-    const searchItems = searchString.split(" ");
-    const isPartialMatch = searchItems.every(function(i) {
-        return itemsToSearch.join().toLowerCase().indexOf(i.toLowerCase()) >= 0;
-    })
-    return isPartialMatch
-}
-
-function findFromCityStateProvince(searchString) {
-  if (searchString) {
-    const cityLookup = _.filter(cityMapping, function (o) { return findPartialMatch([o.city,o.state_ansi,o.province,o.country], searchString) })
-    if (cityLookup && cityLookup.length) {
-      return cityLookup
-    } else {
-      return []
-    }
-  } else {
-    return []
-  }
-}
-
 module.exports = {
   lookupViaCity,
-  findFromCityStateProvince,
   cityMapping
 };
--- test/tests.js
+++ test/tests.js
@@ -2,16 +2,6 @@ const assert = require('assert');
 const cityTimezones = require('../index.js');
 
 describe('City lookup tests', function() {
-    it('finding Chicago', function() {
-      let city = cityTimezones.lookupViaCity('Chicago')
-      console.log('found Chicago by name: ', city)
-      assert.equal(41.82999066, city[0].lat);
-    });
-    it('finding Chicago lower cased', function() {
-      let city = cityTimezones.lookupViaCity('chicago')
-      console.log('found Chicago by name lower case: ', city)
-      assert.equal(41.82999066, city[0].lat);
-    });
     it('return empty for non matching city', function() {
       let city = cityTimezones.lookupViaCity('Foobar')
       assert.deepEqual([], city);
@@ -23,24 +13,6 @@ describe('City lookup tests', function() {
     });
 });
 
-describe('Partial lookup tests', function() {
-  it('finding Springfield MO', function() {
-    let city = cityTimezones.findFromCityStateProvince('springfield mo')
-    console.log('found Springfield by match: ', city)
-    assert.equal(37.18001609, city[0].lat)
-  });
-  it('finding cities matching London', function() {
-    let city = cityTimezones.findFromCityStateProvince('London')
-    console.log('found 6 London\'s by match: ', city)
-    assert.equal(6, city.length);
-  });
-  it('return nothing from nothing', function() {
-    let city = cityTimezones.findFromCityStateProvince('')
-    console.log('found no matches ', city)
-    assert.deepEqual([], city)
-  });
-});
-
 describe('Returns full list of cities from source JSON', function() {
     it('finding all cities', function() {
         const cityMapping = cityTimezones.cityMapping
@@ -48,5 +20,3 @@ describe('Returns full list of cities from source JSON', function() {
         assert(cityMapping.length >= 7323, 'finding all cities');
     });
 });
-
-
