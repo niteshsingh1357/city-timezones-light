ab5cb2a2a3e34cc91c34f4eafe22ea1665e85266
niteshsingh1357 <niteshsingh1357@gmail.com>
2023-02-22 17:13:24 +0545

    re-add partial searching

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 "use strict";
-const _ = require('lodash')
-const cityMapping = require('./data/cityMap.json')
+const _ = require("lodash");
+const cityMapping = require("./data/cityMap.json");
+
+function findPartialMatch(itemsToSearch, searchString) {
+	const searchItems = searchString.split(" ");
+	const isPartialMatch = searchItems.every(function (i) {
+		return itemsToSearch.toLowerCase().includes(i.toLowerCase());
+	});
+	return isPartialMatch;
+}
 
 function lookupViaCity(city) {
-  const cityLookup = _.filter(cityMapping, function (o) { return o.city.toLowerCase() === city.toLowerCase() })
-  if (cityLookup && cityLookup.length) {
-    return cityLookup
-  } else {
-    return []
-  }
+	if (city) {
+		const cityLookup = _.filter(cityMapping, function (o) {
+			return findPartialMatch(o.city, city);
+		});
+		if (cityLookup && cityLookup.length) {
+			return cityLookup;
+		}
+	}
+	return [];
 }
 
 module.exports = {
-  lookupViaCity,
-  cityMapping
+	lookupViaCity,
+	cityMapping,
 };
--- test/tests.js
+++ test/tests.js
@@ -1,22 +1,40 @@
-const assert = require('assert');
-const cityTimezones = require('../index.js');
+const assert = require("assert");
+const cityTimezones = require("../index.js");
 
-describe('City lookup tests', function() {
-    it('return empty for non matching city', function() {
-      let city = cityTimezones.lookupViaCity('Foobar')
-      assert.deepEqual([], city);
-    })
-    it('return multiple matches', function() {
-      let city = cityTimezones.lookupViaCity('Springfield')
-      console.log(city.length + ' matches found for Springfield')
-      assert(city.length > 1, city);
-    });
+describe("City lookup tests", function () {
+	it("return empty for non matching city", function () {
+		let city = cityTimezones.lookupViaCity("Foobar");
+		assert.deepEqual([], city);
+	});
+	it("return multiple matches", function () {
+		let city = cityTimezones.lookupViaCity("Springfield");
+		console.log(city.length + " matches found for Springfield");
+		assert(city.length > 1, city);
+	});
 });
 
-describe('Returns full list of cities from source JSON', function() {
-    it('finding all cities', function() {
-        const cityMapping = cityTimezones.cityMapping
-        console.log('found number of cities: ', cityMapping.length)
-        assert(cityMapping.length >= 7323, 'finding all cities');
-    });
+describe("Partial lookup tests", function () {
+	it("finding Springfield MO", function () {
+		let city = cityTimezones.lookupViaCity("spring");
+		console.log("found Springfield by match: ", city);
+		assert.equal(18, city.length);
+	});
+	it("finding cities matching London", function () {
+		let city = cityTimezones.lookupViaCity("London");
+		console.log("found 6 London's by match: ", city);
+		assert.equal(6, city.length);
+	});
+	it("return nothing from nothing", function () {
+		let city = cityTimezones.lookupViaCity("");
+		console.log("found no matches ", city);
+		assert.deepEqual([], city);
+	});
+});
+
+describe("Returns full list of cities from source JSON", function () {
+	it("finding all cities", function () {
+		const cityMapping = cityTimezones.cityMapping;
+		console.log("found number of cities: ", cityMapping.length);
+		assert(cityMapping.length >= 7323, "finding all cities");
+	});
 });
